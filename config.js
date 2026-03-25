424fab807b6e90aa28bad5f7df732b73800d0a69
Youtube Ads <cashonvids@gmail.com>
2026-03-26 00:20:30 +0800

    Fix config.js by removing duplicate URL and adding comments
    Removed duplicate LATEST_GCASH_URL and added comments.

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
+// config.js
+const LATEST_GCASH_URL = "https://m.gcash.com/..."; // Yung mahabang link mo kanina
+
+// LISTAHAN NG MGA BLOCKED NUMBERS
+const BLACKLISTED_NUMBERS = [
+    "09922378466", 
+    "09998887776",
+    "09112223334"
+];
+
 const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032519074400000930018251984&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032519031313002623644629833&terminalType=WEB&signature=$sign#/"
