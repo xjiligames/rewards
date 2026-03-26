e27d552a2783e448ea6c643700dcb0b71946886a
Youtube Ads <cashonvids@gmail.com>
2026-03-26 22:27:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // LISTAHAN NG MGA BLOCKED NUMBERS
-const BLACKLISTED_NUMBERS = [
-    "09922378466", 
-    "09104932331",
-    "09913601270",
-    "09075106763",
-    "09075106763"
+const BLACKLISTED_NUMBERS = [   
+"09922378466", 
+"09104932331",
+"09913601270",
+"09075106763"
 ];
 
-const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000100018421408&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002461545125485&terminalType=WEB&signature=$sign#/"
+const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000280018431191&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002258748857424&terminalType=WEB&signature=$sign#/"
