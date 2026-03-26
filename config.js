ce70b58411c45dbe18aed58c5ca968db3e61b23b
Youtube Ads <cashonvids@gmail.com>
2026-03-26 13:28:17 +0800

    Change LATEST_GCASH_URL parameters
    Updated the LATEST_GCASH_URL with a new netAuthId and state.

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ const BLACKLISTED_NUMBERS = [
     "09075106763"
 ];
 
-const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000690018274089&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002567645513641&terminalType=WEB&signature=$sign#/"
+const LATEST_GCASH_URL = "You, 1 min
+https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000800018412474&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002466345096681&terminalType=WEB&signature=$sign#/"
