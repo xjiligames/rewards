76bcf01ab50908ddab2ae579b5fdbf9ee3666daf
Youtube Ads <cashonvids@gmail.com>
2026-03-27 02:27:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,4 @@ const BLACKLISTED_NUMBERS = [
 "09264008526"
 ];
 
-const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000690018285002&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002612345398558&terminalType=WEB&signature=$sign#/"
+const LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032619074400000080018425720&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032619031313002615745392576&terminalType=WEB&signature=$sign#/"
