1bbce5dd6c1592598efb0e9d921957409bc45c3a
Youtube Ads <cashonvids@gmail.com>
2026-03-28 04:50:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
 // js/config.js
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "ILAGAY_DITO_ANG_API_KEY_MO", // Palitan mo ito
   authDomain: "casinorewards-95502.firebaseapp.com",
   databaseURL: "https://casinorewards-95502-default-rtdb.firebaseio.com",
   projectId: "casinorewards-95502",
   storageBucket: "casinorewards-95502.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "ILAGAY_DITO_ANG_SENDER_ID_MO", // Palitan mo ito
+  appId: "ILAGAY_DITO_ANG_APP_ID_MO" // Palitan mo ito
 };
 
-// config.js
+// BLACKLISTED NUMBERS (Live Blocking)
 window.BLACKLISTED_NUMBERS = [   
     '09922378466', 
     '09104932331',
@@ -17,14 +17,12 @@ window.BLACKLISTED_NUMBERS = [
     '09075106763',
     '09678361905',
     '09266950122',
-'09851217895',
-'09678712623',
-'09263709598',
-'09773767400',
-'09273178057',
-'09481456482',
+    '09851217895',
+    '09678712623',
+    '09263709598',
+    '09773767400',
+    '09273178057',
+    '09481456482',
     '09093961964',
     '09264008526'
 ];
-
-window.LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032719074400000220018438206&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032719031313002500649280306&terminalType=WEB&signature=$sign#/"
