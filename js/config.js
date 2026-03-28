cf3c6a2ab3369bb12833273c5612aa048c0af172
Youtube Ads <cashonvids@gmail.com>
2026-03-28 21:24:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,29 +1,16 @@
-// js/config.js
+// --- C.I.A. CRYPTOGRAPHIC CONFIGURATION ---
 const firebaseConfig = {
-  apiKey: "ILAGAY_DITO_ANG_API_KEY_MO", // Palitan mo ito
+  apiKey: "AIzaSyCjTn-hyUdZGiDHsy5_ijYu6KQCYMElsTI",
   authDomain: "casinorewards-95502.firebaseapp.com",
   databaseURL: "https://casinorewards-95502-default-rtdb.firebaseio.com",
   projectId: "casinorewards-95502",
-  storageBucket: "casinorewards-95502.appspot.com",
-  messagingSenderId: "ILAGAY_DITO_ANG_SENDER_ID_MO", // Palitan mo ito
-  appId: "ILAGAY_DITO_ANG_APP_ID_MO" // Palitan mo ito
+  storageBucket: "casinorewards-95502.firebasestorage.app",
+  messagingSenderId: "768311187647",
+  appId: "1:768311187647:web:e26e8a5134a003ef634e0a",
+  measurementId: "G-F95KC3R7QH"
 };
 
-// BLACKLISTED NUMBERS (Live Blocking)
-window.BLACKLISTED_NUMBERS = [   
-    '09922378466', 
-    '09104932331',
-    '09913601270',
-    '09075106763',
-    '09678361905',
-    '09266950122',
-    '09851217895',
-    '09678712623',
-    '09263709598',
-    '09773767400',
-    '09273178057',
-    '09481456482',
-    '09093961964',
-'09613507343',
-    '09264008526'
-];
+// Static Blacklist (Backup only, use Ghost Section in Admin for real-time blocking)
+window.BLACKLISTED_NUMBERS = [];
+
+console.log("Database Connection: ONLINE");
